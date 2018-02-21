485f6bf1f700996304236b67337f6211c914c5d1
navalsteed <caichaoyu20@gmail.com>
2018-02-21 12:35:02 +0100

    Update main.js

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,12 @@
 $(function() {
 
   // your code will go here
+  $.ajax({
+  url:'https://www.codeschool.com/users/4443145.json',
+  datatype:'jsonp',
+  success:function(response){
+  console.log('rsponse',response);
+  }  
+  });
 
 });
